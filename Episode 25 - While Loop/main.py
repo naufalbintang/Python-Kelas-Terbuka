c956d2be93b5ffc81079c509ea57c607a4e986a3
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-06 15:21:06 +0700

    -a

---
+++ Episode 25 - While Loop/main.py
@@ -0,0 +1,11 @@
+# while loop
+
+angka = 0
+print(f"Angka sekarang -> {angka}")
+
+while angka < 5:
+    angka += 1
+    print(f"Angka sekarang -> {angka}")
+    print("otong kece!")
+    
+print("cukup")
