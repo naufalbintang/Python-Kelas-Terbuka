1fe3dedde112b604a1d9fead26fcfa154cfc8b1a
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-06 15:10:05 +0700

    Episode 24 - For Loop

---
+++ Episode 24 - For Loop/main.py
@@ -0,0 +1,23 @@
+# Perulangan (Loop)
+
+# dengan list
+angka_list = [1, 2, 3, 4, 5]
+for i in angka_list:
+    print(f"i sekarang -> {i}")
+    
+print("Akhir dari program\n")
+    
+# dengan range
+angka_range = range(2,11)
+for i in angka_range:
+    print(f"i sekarang -> {i}")
+    
+print("Akhir dari program\n")
+
+# dengan string
+data_str = "saya ganteng abiezz"
+
+for huruf in data_str:
+    print(huruf)
+    
+print("Akhir dari program\n")
