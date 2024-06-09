5eb0e68c593bd43c207e466f97ec77dfda5724f1
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-09 13:08:04 +0700

    Episode 35 - Looping List dan Enumerate/

---
+++ Episode 35 - Looping List dan Enumerate/main.py
@@ -0,0 +1,44 @@
+# looping dari list
+
+# for loop
+print("for loop")
+kumpulan_angka = [4, 3, 2, 5, 6, 1]
+
+for angka in kumpulan_angka:
+    print(f"angka = {angka}")
+    
+peserta = ["ucup", "otong", "dadang", "diding", "dudung"]
+for nama in peserta:
+    print(f"nama = {nama}")
+    
+# for loop dan range
+print("\nfor loop dan range")
+kumpulan_angka = [10, 5 , 4 , 2, 6, 5]
+
+panjang = len(kumpulan_angka)
+for i in range(panjang):
+    print(f"angka = {kumpulan_angka[i]}")
+    
+# while
+print("\nwhile")
+panjang = len(kumpulan_angka)
+i = 0
+while i < panjang:
+    print(f"angka = {kumpulan_angka[i]}")
+    i += 1
+    
+# list comprehension
+print("\nlist comprehension")
+data = ["ucup", 1, 2, 3, "otong"]
+[ print(f"data = {i}") for i in data]
+
+angka = [10, 5 , 4 , 2, 6, 5]
+angka_kuadrat = [i**2 for i in angka]
+print(angka_kuadrat)
+
+# enumerate
+print("\nenumerate")
+data_list = ["ucup", 1, 2, 3, "otong"]
+
+for index, data in enumerate(data_list):
+    print(f"index = {index}, data = {data}")
