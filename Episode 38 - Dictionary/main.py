694abe7badd7cb4f42bced411ea7aaa4a095b755
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-10 12:56:30 +0700

    Episode 38 - Dictionary/

---
+++ Episode 38 - Dictionary/main.py
@@ -0,0 +1,40 @@
+# list -> array, mengakses data dengan 
+# menggunakan index
+
+data_list = ["ucup", "otong", "dudung"]
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# menggunakan identifier -> key
+
+data_dict = {
+    "key": "value",
+    "cp": "ucup",
+    "tg": "otong",
+    "dg": "dudung",
+    "nmbr": 100,
+    "list": data_list,
+}
+
+print(data_dict["tg"])
+print(data_dict["nmbr"])
+print(data_dict["list"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
