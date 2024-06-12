589cbd80c3b2fab55d9cce45a2cd8e6f73bf0655
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-12 16:03:48 +0700

    modified test.py and test2.py

--- test.py
+++ test.py
@@ -1,22 +1,87 @@
-# faktorial
+import datetime
 
-angka = int(input("Masukkan angka: "))
-data_list = []
-hasil = 1
+game1 = {
+    'nama': 'CSGO',
+    'publisher': 'Valve',
+    'steam': True,
+    'peak_player': 1_519_457,
+    'tanggal_rilis': datetime.datetime(2012, 8, 21),
+}
 
-if angka != 0:
-    while True:
-        data_list.append(angka)
-        angka -= 1
-        
-        # break
-        if angka == 0:
-            break
+game2 = {
+    'nama': 'Genshin Impact',
+    'publisher': 'Hoyolab',
+    'steam': False,
+    'peak_player': 63_000_000,
+    'tanggal_rilis': datetime.datetime(2020, 9, 28)
+}
 
-    for i in data_list:
-        print(i)
-        hasil = hasil * i
+game3 = {
+    'nama': 'Honkai Star Rail',
+    'publisher': 'Hoyolab',
+    'steam': False,
+    'peak_player': 24_000_000,
+    'tanggal_rilis': datetime.datetime(2023, 4, 26)
+}
 
-    print(hasil)        
-else:
-    print("MasukKkan angka yang valid!")
+game4 = {
+    'nama': 'Dota 2',
+    'publisher': 'Valve',
+    'steam': True,
+    'peak_player': 1_295_114,
+    'tanggal_rilis': datetime.datetime(2009, 7, 9)
+}
+game5 = {
+    'nama': 'League of Legends',
+    'publisher': 'Riot Games',
+    'steam': False,
+    'peak_player': 8_000_000,
+    'tanggal_rilis': datetime.datetime(2009, 10, 27)
+}
+
+data_games = {
+    'GAME001': game1,
+    'GAME002': game2,
+    'GAME003': game3,
+    'GAME004': game4,
+    'GAME005': game5,
+}
+
+print(f'{'key':<8} {'Nama':<18} {'Publisher':<11} {'Steam':<6} {'Peak Players':<13} {'Tanggal Rilis':<8}')
+print(f'-'*74)
+
+for KEY in data_games:
+    NAMA = data_games[KEY]['nama']
+    PUBLISHER = data_games[KEY]['publisher']
+    STEAM = data_games[KEY]['steam']
+    PEAK_PLAYERS = data_games[KEY]['peak_player']
+    TANGGAL_RILIS = data_games[KEY]['tanggal_rilis'].strftime('%x')
+    
+    print(f'{KEY:<8} {NAMA:<18} {PUBLISHER:<11} {STEAM:^6} {PEAK_PLAYERS:<13,.0f} {TANGGAL_RILIS:<8}')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,57 @@
+import datetime
+import os
+import string
+import random
+
+data_games_template = {
+    'nama': 'nama',
+    'publisher': 'publisher',
+    'peak': 1_000_000,
+    'rilis': datetime.datetime(1111, 1, 11)
+}
+
+data_games = {}
+
+while True:
+    os.system('clear')
+    print(f'{'DATA GAMES':^20}')
+    print('-'*20)    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # break
+    is_done = input('Apakah sudah selesai (y/n)? ')
+    if is_done == 'y':
+        break
+    else:
+        None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
