19178dc2a6b43a8daf91e016d0d31b58f76eb3a8
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-13 14:09:42 +0700

    Episode 44 - Pengenalan Fungsi/

---
+++ Episode 44 - Pengenalan Fungsi/main.py
@@ -0,0 +1,37 @@
+'''Membuat Fungsi'''
+
+def hello_world():
+    print('Hello World!')
+    print('Kepada Ucup Surucup')
+    print('dan juga kepada otong surotong')
+
+hello_world()
+hello_world()
+
+def fungsi():
+    '''pemanggilan fungsi harus setelah dibuat'''
+    print('Ini adalah Fungsi')
+    
+fungsi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
