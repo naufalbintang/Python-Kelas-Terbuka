8cfce85882167c5b40f12419b52c2bbdec92d598
Naufal Bintang Pradana Himawan <naufalbintangpradana@gmail.com>
2024-06-17 15:52:57 +0700

    rata2.py

---
+++ rata2.py
@@ -0,0 +1,46 @@
+import os
+
+daftar_nilai = {
+    'PU': 601.28,
+    'PK': 710.42,
+    'PPU': 756.46,
+    'PBM': 684.21,
+    'LBI': 649.63,
+    'LBE': 743.87,
+    'PM': 677.61,
+}
+
+os.system('cls')
+print(f'{'RATA - RATA NILAI UTBK':^24}')
+print('='*24)
+for key in daftar_nilai:
+    nilai = daftar_nilai[key]
+    print(f'{key}\t\t= {nilai}')
+
+def rata2_nilai(pu, pk, ppu, pbm, lbi, lbe, pm):
+    hasil = (pu + pk + ppu + pbm + lbi + lbe + pm) / 7
+    return hasil
+
+rata_rata = rata2_nilai(daftar_nilai['PU'], daftar_nilai['PK'], daftar_nilai['PPU'], daftar_nilai['PBM'], daftar_nilai['LBI'], daftar_nilai['LBE'], daftar_nilai['PM'])
+print(f'Rata - rata\t= {rata_rata:.2f}\n\n\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
