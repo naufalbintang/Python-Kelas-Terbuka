088f6363900312249627a73de7b7cde64b072788
Naufal <naufalbintangpradana@gmail.com>
2024-07-14 06:39:39 +0700

    menghapus beberapa file yang tidak penting

--- test.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        angka:float = float(input('Masukkan angka: '))
-        print(angka)
-        break
-    except:
-        print('Tolong masukkan angka.')
--- test3.py
+++
@@ -1,8 +0,0 @@
-a: dict[str] = {
-    'luas',
-    'keliling',
-    'volume'
-}
-
-for index, operasi in enumerate(a):
-    print(f'{index}. {operasi}')
